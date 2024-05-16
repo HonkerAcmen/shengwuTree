00feb9d62c64f1b6fb8726f6ad5aa26e5ab9a359
Mark Yang <haker0x04@163.com>
2024-05-16 20:56:06 +0800

    完成head样式

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-
-
+import '/public/css/base.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-       home 
+    <link rel="stylesheet" href="/css/layout.css">
+    <div class="head">
+        <div class="logo"><img src="/images/logo.png" alt=""></div>
+        <div class="buttons">
+            <el-button size="large">登录</el-button>
+            <el-button type="primary" size="large">注册</el-button>
+        </div>
     </div>
 </template>
 
